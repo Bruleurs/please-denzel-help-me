3ca5acbe0b52aa960548a4408b6db08c52f37aee
Bruleurs <46087521+Bruleurs@users.noreply.github.com>
2020-05-15 17:44:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 // Q1. Create a variable called `denzel` - that can't be redeclared - and assign it the string 'please denzel, help me'
 
+const denzel = "please denzel, help me";
+
 // Q2. Use the variable `actor` to create a new variable called `inspirational` that is assigned the string "Is Denzel Washington an inspirational actor?"
 const actor = 'Denzel Washington';
+var inspirational = 'Is ' + actor + ' an inspirational actor?';
 
 // Q3  Create an object called `family` and give it the following properties:
 // family has been `married` on "1983" (Number)
 // family is not `presidentOfTheUnitedStates` (boolean)
 // family has `children` called "John David", "Katia", "Olivia" and "Malcolm" (array of strings)
+
+var Family = {
+  'Married':1983,
+  'presidentOfTheUnitedStates':false,
+  'Children':['John David', 'Katia', 'Olivia', 'Malcolm']
+  }
+  
 
 // Q4. Change
 // * the `name` stage to "A Raisin in the Sun"
@@ -17,10 +27,14 @@ const stage = {
   'year': '2010'
 };
 
+stage.name = "A Raising in the Sun"
+
 // Q5. Change the `movie` "glory" to "Training Day" using square bracket notation
 const awards = {
   'movie': 'glory'
 };
+
+awards['movie'] = 'Training Day';
 
 // Q6. Create an object called "earlywork"
 // The key should be the movie name and the value the release year
@@ -30,6 +44,15 @@ const awards = {
 // * Power (1986)
 // * Cry Freedom (1987)
 // * For Queen and Countryy (1988)
+
+earlywork = {
+  "Carbon Copy" : "1981",
+  "A Soldier Story" : "1984",
+  "Power":"1986",
+  "CryFreedom":"1987",
+  "For Queen and Countryy":"1988"
+  }
+  
 
 // Q7. Return a new array from `debaters` with all item in uppercase
 const debaters = ['we do', 'what we have to do', 'in order to do', 'what we want to do'];
